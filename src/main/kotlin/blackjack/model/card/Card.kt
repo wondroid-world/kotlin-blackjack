c922cf3115d6f1572a8bdaf5dfe7d00074e12ffd
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-03-13 16:06:40 +0900

    refactor: 사용 하지 않는 줄 삭제

--- src/main/kotlin/blackjack/model/card/Card.kt
+++ src/main/kotlin/blackjack/model/card/Card.kt
@@ -1,8 +1,5 @@
 package blackjack.model.card
 
-import java.util.Objects
-import kotlin.jvm.internal.Ref.ObjectRef
-
 data class Card(
     val shape: CardShape,
     val denomination: Denomination,
