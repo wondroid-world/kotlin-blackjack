a691a027bea47e8b93129862656229cb823b8072
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-03-14 08:55:20 +0900

    refactor: player 생성자 네이밍 변경 & 이름 테스트 추가

--- src/main/kotlin/blackjack/model/participant/Player.kt
+++ src/main/kotlin/blackjack/model/participant/Player.kt
@@ -1,22 +1,13 @@
 package blackjack.model.participant
 
-import blackjack.model.PlayerBehavior
-import blackjack.model.card.Cards
-import blackjack.model.card.CardsStatus
+import blackjack.model.card.HandCards
 
 class Player(
     name: String,
-    cards: Cards = Cards(emptyList()),
-) : Participant(name, cards) {
+    handCards: HandCards = HandCards(mutableListOf()),
+) : Participant(name, handCards) {
     init {
         require(name != "딜러") { "플레이어는 딜러라는 이름을 가질 수 없습니다." }
         require(name.length in 1..5) { "플레이어는 1에서 5사이 길이의 이름만 가질 수 있습니다." }
-    }
-
-    fun isPossibleToGetCard(playerBehavior: PlayerBehavior): Boolean {
-        if (cards.getStatus() != CardsStatus.BUST && playerBehavior == PlayerBehavior.HIT) {
-            addCard()
-        }
-        return false
     }
 }
