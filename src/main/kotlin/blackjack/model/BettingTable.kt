037e2eef9418249dbc37ff385ef58fa2df590c36
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-03-15 20:24:00 +0900

    feat: BettingMoney를 계산하는 클래스 구현

---
+++ src/main/kotlin/blackjack/model/BettingTable.kt
@@ -0,0 +1,18 @@
+package blackjack.model
+
+import blackjack.model.participant.Participant
+import blackjack.model.state.Finish
+
+class BettingTable(
+    val participant: Participant,
+    private val bettingAmount: Money,
+) {
+    fun getProfit(): Money {
+        val participantState = participant.state
+        return if (participantState is Finish) {
+            participantState.profit(bettingAmount)
+        } else {
+            throw IllegalStateException("참가자의 상태가 끝나지 않았습니다.")
+        }
+    }
+}
