cd1e0f4c80bd7a8e932b2c96449896f34436c4b7
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-03-16 13:40:05 +0900

    feat: 블랙잭 룰 상수화

---
+++ src/main/kotlin/blackjack/constants/BlackjackRule.kt
@@ -0,0 +1,14 @@
+package blackjack.constants
+
+object BlackjackRule {
+    const val DEALER_HIT_SCORE = 16
+    const val NAME_LENGTH_MIN = 1
+    const val NAME_LENGTH_MAX = 5
+    const val MIN_PLAYERS = 1
+    const val MAX_PLAYERS = 8
+    const val BLACKJACK_PROFIT = 1.5f
+    const val STAY_PROFIT = 1f
+    const val BUST_PROFIT = -1f
+    const val BLACKJACK_SCORE = 21
+    const val FIRST_CARD_COUNT = 2
+}
