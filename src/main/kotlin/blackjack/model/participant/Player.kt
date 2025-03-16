07ef52ae5062de34ab8cef60bceeb91d9d625181
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-03-16 13:58:02 +0900

    feat: player 기능 구현

--- src/main/kotlin/blackjack/model/participant/Player.kt
+++ src/main/kotlin/blackjack/model/participant/Player.kt
@@ -1,20 +1,16 @@
 package blackjack.model.participant
 
-import blackjack.model.GameResult
-import blackjack.model.card.Cards
+import blackjack.constants.BlackjackRule.NAME_LENGTH_MAX
+import blackjack.constants.BlackjackRule.NAME_LENGTH_MIN
+import blackjack.model.state.StartGame
+import blackjack.model.state.State
 
 class Player(
     name: String,
-    cards: Cards = Cards(emptyList()),
-) : Participant(name, cards) {
+    state: State = StartGame(),
+) : Participant(name, state) {
     init {
         require(name != "딜러") { "플레이어는 딜러라는 이름을 가질 수 없습니다." }
-        require(name.length in 1..5) { "플레이어는 1에서 5사이 길이의 이름만 가질 수 있습니다." }
-    }
-
-    lateinit var result: GameResult
-
-    fun updateResult(dealerGameResult: GameResult) {
-        result = GameResult.reversed(dealerGameResult)
+        require(name.length in NAME_LENGTH_MIN..NAME_LENGTH_MAX) { "플레이어는 1에서 5사이 길이의 이름만 가질 수 있습니다." }
     }
 }
