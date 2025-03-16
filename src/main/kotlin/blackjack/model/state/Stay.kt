49555310b5c14aa4d8a207123a40d2fe1937130c
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-03-16 13:50:36 +0900

    feat: stay 클래스 구현

---
+++ src/main/kotlin/blackjack/model/state/Stay.kt
@@ -0,0 +1,13 @@
+package blackjack.model.state
+
+import blackjack.constants.BlackjackRule.STAY_PROFIT
+import blackjack.model.card.Hand
+
+class Stay(
+    override val hand: Hand,
+) : Finish(hand) {
+    override val profitRate: Float
+        get() = STAY_PROFIT
+}
+
+
