b3f0bce846acf53a9591ccec9f1a4f4055533ca8
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-03-16 13:56:33 +0900

    feat: 참가자 기능 구현

--- src/main/kotlin/blackjack/model/participant/Participant.kt
+++ src/main/kotlin/blackjack/model/participant/Participant.kt
@@ -1,11 +1,23 @@
 package blackjack.model.participant
 
 import blackjack.model.card.Card
-import blackjack.model.card.Cards
+import blackjack.model.state.Hit
+import blackjack.model.state.StartGame
+import blackjack.model.state.State
 
 abstract class Participant(
     val name: String,
-    val cards: Cards,
+    private var _state: State = StartGame(),
 ) {
-    fun addCard(card: Card) = cards.add(card)
+    val state: State get() = _state
+
+    fun updateState(card: Card) {
+        _state = _state.draw(card)
+    }
+
+    fun stop() {
+        if (_state is Hit) {
+            _state = (_state as Hit).stop()
+        }
+    }
 }
