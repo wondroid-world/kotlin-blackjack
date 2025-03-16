dacf67ea71a720cdcd1d04350d9a1c4fb8f7ea2d
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-03-16 13:45:40 +0900

    feat: blackjackGame class 구현

---
+++ src/main/kotlin/blackjack/model/BlackjackGame.kt
@@ -0,0 +1,45 @@
+package blackjack.model
+
+import blackjack.constants.BlackjackRule.FIRST_CARD_COUNT
+import blackjack.model.card.CardDeck
+import blackjack.model.participant.Dealer
+import blackjack.model.participant.Player
+import blackjack.model.participant.Players
+
+class BlackjackGame(
+    private val players: Players,
+    private val dealer: Dealer,
+) {
+    private val cardDeck = CardDeck.create()
+
+    fun firstTurn() {
+        repeat(FIRST_CARD_COUNT) {
+            giveCard()
+        }
+    }
+
+    private fun giveCard() {
+        dealer.updateState(cardDeck.pickCard())
+        players.value.forEach { player ->
+            player.updateState(cardDeck.pickCard())
+        }
+    }
+
+    fun dealerTurn(): Boolean {
+        while (dealer.canDraw()) {
+            dealer.updateState(cardDeck.pickCard())
+            return true
+        }
+        return false
+    }
+
+    fun hitOrStay(
+        player: Player,
+        playerBehavior: PlayerBehavior,
+    ) {
+        when (playerBehavior) {
+            PlayerBehavior.HIT -> player.updateState(cardDeck.pickCard())
+            PlayerBehavior.STAY -> player.stop()
+        }
+    }
+}
