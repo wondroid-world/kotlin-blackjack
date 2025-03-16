a5ed17281a29f1a1e28c015ca25dd0dcdd74b11a
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-03-16 13:50:04 +0900

    feat: start class 구현

---
+++ src/main/kotlin/blackjack/model/state/StartGame.kt
@@ -0,0 +1,13 @@
+package blackjack.model.state
+
+import blackjack.model.card.Card
+import blackjack.model.card.Hand
+
+class StartGame(
+    override val hand: Hand = Hand(emptyList()),
+) : State {
+    override fun draw(card: Card): Hit {
+        val newHand = hand + card
+        return Hit(newHand)
+    }
+}
